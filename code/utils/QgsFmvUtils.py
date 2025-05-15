0a669c1e98edff992203fab4265087633c1315a5
Baud Rémy <remy.baud@swisstopo.ch>
2025-05-15 12:33:39 +0200

    fixe ffmpeg path

--- code/utils/QgsFmvUtils.py
+++ code/utils/QgsFmvUtils.py
@@ -852,7 +852,7 @@ def _spawn(cmds, t="ffmpeg"):
         cmds.insert(0, ffprobe_path)
     
     
-    qgsu.showUserAndLogMessage("", "spawned : " + " ".join(cmds), onlyLog=True)
+    #qgsu.showUserAndLogMessage("", "spawned : " + " ".join(cmds), onlyLog=True)
     
     return subprocess.Popen(cmds, shell=windows, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                             bufsize=0,
